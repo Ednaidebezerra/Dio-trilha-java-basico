959ddabd39f8b175b654678432a31c76716afbec
Ednaide <ednaidebezerra2022@outlook.com>
2024-06-19 13:02:45 -0300

    ultima atualização

--- conta-banco/src/main/java/org/yourcompany/yourproject/ContaTerminal.java
+++ conta-banco/src/main/java/org/yourcompany/yourproject/ContaTerminal.java
@@ -7,62 +7,84 @@ public class ContaTerminal {
 
     public static void main(String[] args) {
 
-        int numeroConta = 1021;
-        String agencia = "067-8";
-        String nomeCliente = "Mario Andrade";
+        int numeroConta;
+        String agencia;
+        String nomeCliente;
         Double saldo = 237.48;
 
-       Double saque;
-       double deposito;
+        Double valorSaque;
+        Double valorDeposito;
 
         Scanner leitura = new Scanner(System.in);
 
-         System.out.println("Por favor, digite o número da conta !");
+        System.out.println("Por favor, digite o número da conta !");
 
-            numeroConta = leitura.nextInt();
-            System.out.println(" Conta: " + numeroConta);
-
-         System.out.println("Por favor, digite o número da Agencia!");
-
-            agencia = leitura.next();
-            System.out.println(" Agencia: " + agencia);
-
-         System.out.println("Por favor, digite seu nome completo!");
-
-            nomeCliente = leitura.next();
-            System.out.println(" Nome:" + nomeCliente);
-
-         System.out.println(" Olá, " + nomeCliente + "Obrigado por criar uma conta em nosso banco, sua Agencia é:" +
-         agencia + "Conta" + numeroConta + " E seu saldo já está disponível para saque." );
+        numeroConta = leitura.nextInt();
+        System.out.println(" Conta: " + numeroConta);
+        System.out.println("");
 
 
-         int opçao = 0;
-          String menu = """
+        System.out.println("Por favor, digite o número da Agencia!");
+
+        agencia = leitura.next();
+        System.out.println(" Agencia: " + agencia);
+        System.out.println("");
+
+        System.out.println("Por favor, digite seu nome completo!");
+        nomeCliente = leitura.next();
+        System.out.println(" Nome:" + nomeCliente);
+        System.out.println("");
+
+        System.out.println(" Olá, " + nomeCliente + "Obrigado por criar uma conta em nosso banco, sua Agencia é:"
+                + agencia + "Conta" + numeroConta + " E seu saldo já está disponível para saque.");
+        System.out.println("");
+         
+        int opçao = 0;
+        String menu = """
                     ** Digite uma opção ** 
                     1- Consultar saldo
                     2- Depositar
                     3- Sacar
                     4- Sair
                     """;
-         while ( opçao != 4) {
-            System.out.println( menu);
+        while (opçao != 4) {
+            System.out.println(menu);
             opçao = leitura.nextInt();
 
-         } if ( opçao == 1){
-            System.out.println("Seu saldo é:" + saldo);
+            switch (opçao) {
+                case 1:
+                    System.out.println("Seu saldo é:" + saldo);
+                    System.out.println("");
+                    break;
 
-         } else if ( opçao == 2) {
-            System.out.println(" Qual valor deseja sacar ?");
-            Double valor = leitura.nextDouble();
+                case 2:
+                    System.out.println(" Qual valor deseja Depositar ?");
+                    valorDeposito = leitura.nextDouble();
+                    saldo = saldo + valorDeposito;
+                    System.out.println("Seu novo saldo é:" + saldo);
+                    System.out.println("");
+                    break;
 
-            System.out.println( "Seu novo saldo é:" + (saldo + valor));
+                case 3:
+                    System.out.println("Digite o valor que deseja sacar:");
+                    valorSaque = leitura.nextDouble();
 
-         } else if (opçao== 3){
-             System.out.println("Digite o valor que deseja sacar:");
+                    if (valorSaque > saldo) {
+                        System.out.println(" Saldo insuficiente");
+                        System.out.println("");
+                        break;
+                    } else if (valorSaque < saldo) {
+                        saldo = saldo - valorSaque;
+                        System.out.println("Saque realizado com sucesso, seu novo saldo é:" + (saldo));
+                        System.out.println("");
+                    }
 
-         }
-         
-    
+                case 4:
+                    System.out.println("Obrigada por usar nossos serviços!");
+
+            }
+
+        }
 
     }
 }
