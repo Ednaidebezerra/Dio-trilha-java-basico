dfe5b463a80cdffac770ef445094ffc48a34c390
Ednaide <ednaidebezerra2022@outlook.com>
2024-06-18 22:29:50 -0300

    atualizando

--- conta-banco/src/main/java/org/yourcompany/yourproject/ContaTerminal.java
+++ conta-banco/src/main/java/org/yourcompany/yourproject/ContaTerminal.java
@@ -10,18 +10,20 @@ import java.util.Scanner;
  * @author Ednaide Bezerra
  */
 public class ContaTerminal {
-
+// declaração de variaveis da classe principal
     int numeroConta = 1021;
     String agencia = "067-8";
     String nomeCliente = "Mario Andrade";
      Double saldo = 237.48;
 
-    public static void main(String[] args) {
+    public static  void main(String[] args) {
 
         System.out.println("Por favor, digite o número da agencia !");
      
-        Scanner var1 = new Scanner ( System.in);
-        int numero = var1.nextInt();
+        Scanner leitura = new Scanner ( System.in);
+        double numero = leitura.nextDouble();
+      
+        
 
     }
 }
